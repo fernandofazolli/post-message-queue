958062f4eaad9ff0290d2243e644c3cb901f80c6
fernandofazolli <fernandofazolli@gmail.com>
2020-06-23 14:45:17 -0300

    Adicionando feature

--- extension.js
+++ extension.js
@@ -14,24 +14,46 @@ function activate(context) {
 	
 	console.log('Congratulations, your extension "post-message-queue" is now active!');
 
-	let disposable = vscode.commands.registerCommand('post-message-queue.postMessage', function () {
-    let message = messageToSend();
-		axios.post(
-			'http://admin:admin@localhost:8161/api/message?destination=queue://'+ queueName(),
-			message, 
-			  config
-		  ).then(() => {
-			vscode.window.showInformationMessage('Activemq-Post [ ' + message+' ]');
-		  })
-		  .catch((error) => {
-			console.error(error)
-			vscode.window.showErrorMessage('Activemq-Post Error [' + error + ']');
-		  })		
+	let disposable = vscode.commands.registerCommand('post-message-queue.postMessage',async function() {
+	let message = messageToSend();
+
+	for (let i = 0; i < message.length; i++) {
+		await sendToQueue(message[i])
+	}
+	  
+	//   Promise.all(message.map(info => sendToQueue(info))).then(results => {
+	// 	  console.log(results);
+	//   }).catch(err => {
+	// 	  console.log(err);
+	//   });
+
+
+	// message.forEach(async (item) => {	
+	// 	console.log(item); 
+	// 	await sendToQueue(item); 	
+	// 	});	
 	});
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+
+
+var  sendToQueue = (item) => {
+	return axios.post(
+		'http://admin:admin@localhost:8161/api/message?destination=queue://'+ queueName(),
+		item, 
+		  config
+	  ).then(() => {			 
+		vscode.window.showInformationMessage('Activemq-Post [ ' + item+' ]');
+		return
+	  })
+	  .catch((error) => {
+		console.error(error)
+		vscode.window.showErrorMessage('Activemq-Post Error [' + error + ']');
+		return
+	  })
+}
 
 var queueName = () => {
 	let fileName = vscode.window.activeTextEditor.document.fileName.split('/');
@@ -39,7 +61,7 @@ var queueName = () => {
 }
 
 var messageToSend = () => {
-	return vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection);
+	return vscode.window.activeTextEditor.document.getText(vscode.window.activeTextEditor.selection).split("\n");
 }
 
 // this method is called when your extension is deactivated
